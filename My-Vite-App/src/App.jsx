b7d9eaba7b3afb81685b8ca460cad2f107a8723c
RAAQ-SOFTTEK <roberto.ayala@softtek.com>
2025-03-09 21:07:53 -0600

    Abres la aplicacion y te redirije a login

--- My-Vite-App/src/App.jsx
+++ My-Vite-App/src/App.jsx
@@ -4,35 +4,30 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import Login from './components/Login'; // Asegúrate de que la ruta sea correcta
 import Navbar from './components/Navbar'; // Importa el componente Navbar
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'; // Importa Navigate
 import { AuthProvider } from './host/useAuth';
 import { useAuth } from './host/useAuth';
 
-
 function App() {
-
- 
-
   return (
     <BrowserRouter>
-    {/* Agrega el componente Navbar */}
-
-    <AuthProvider>
-
-      <Routes>
-
-        <Route
-          path="/login"
-          element={<Login mail={"roberto.ayala@softtek.com"}></Login>}
-        />
-
-        <Route
-          path='/Navbar'
-          element={<Navbar></Navbar>}
-        />
-      </Routes>
-    
-    </AuthProvider>
+      {/* Agrega el componente Navbar */}
+      <AuthProvider>
+        <Routes>
+          <Route
+            path="/login"
+            element={<Login mail={"roberto.ayala@softtek.com"} />}
+          />
+          <Route
+            path='/Navbar'
+            element={<Navbar />}
+          />
+          <Route
+            path="/"
+            element={<Navigate to="/login" />} // Redirige a /login
+          />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
